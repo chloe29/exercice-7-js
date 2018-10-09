08b8c556d8a1a23c73fdd3aecf488a124944af6c
lavisse <chloe.lavisse80@gmail.com>
2018-10-09 11:52:40 +0200

    envoi correction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function calc(){
   var shoeSize = document.getElementById('shoeSize').value;
   var yearOfBirth = document.getElementById('yearOfBirth').value;
-  var result = ((((shoeSize * 2) + 5) * 50) - yearOfBirth) + 1766;
-    if(!isNaN(shoeSize) && !isNan(yearOfBirth)){
-      alert(result);
+  var result = ((((shoeSize * 2) + 5) * 50) - yearOfBirth) + 1768;
+    if(!isNaN(shoeSize) && !isNaN(yearOfBirth)){
+      return result;
     }else{
       alert('Veuillez renseigner les champs correctement !');
     }
